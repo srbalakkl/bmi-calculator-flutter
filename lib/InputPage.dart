2f75bf5e5e89327719a98ff64e53a5ec493bb22e
SR.Bala <sr.bala.kkl@gmail.com>
2021-06-07 16:05:14 +0530

    ReusableClass() is added

--- lib/InputPage.dart
+++ lib/InputPage.dart
@@ -12,17 +12,58 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
       ),
-      body: Center(
-        child: Text('Body Text'),
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            Row(
+              children: [
+                Expanded(
+                  child: ReusableCard(),
+                ),
+                Expanded(child: ReusableCard()),
+              ],
+            ),
+            Expanded(child: ReusableCard()),
+            Row(
+              children: [
+                Expanded(child: ReusableCard()),
+                Expanded(
+                  child: ReusableCard(),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
       floatingActionButton: Theme(
-        data:ThemeData(
+        data: ThemeData(
           accentColor: Colors.white70,
         ),
         child: FloatingActionButton(
           child: Icon(Icons.add),
         ),
       ),
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  const ReusableCard({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      //TODO:use decorator only if colors are added inside in it.
+
+      decoration: BoxDecoration(
+        color: Color(0xFF1D1E20),
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      // width: 100,
+      height: 100,
     );
   }
 }
